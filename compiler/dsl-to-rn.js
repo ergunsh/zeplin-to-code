71062aa44068954b66ab3c8e3036289ecd8e6918
ergunsh <erdogmusergun@gmail.com>
2019-05-07 14:09:28 +0300

    Implement dsl-to-rn

--- compiler/dsl-to-rn.js
+++ compiler/dsl-to-rn.js
@@ -1,6 +1,7 @@
+const prettier = require("prettier");
 const styleValueMapping = require("./style-value-mapping");
 const tokens = require("./tokens");
-const example = `.{.block.static.row.nowrap.}`;
+const example = `.{.block.static.row.nowrap.|.block.static.row.nowrap.{.block.static.row.nowrap.}.}`;
 
 function getRuleFromToken(token) {
     const properties = Object.keys(styleValueMapping);
@@ -21,61 +22,89 @@ function getRuleFromToken(token) {
 
 function convertTokenToStyle(token) {
     const { property, value } = getRuleFromToken(token);
-    return `${property}: "${value}"`;
+    return `"${property}": "${value}"`;
 }
 
-function generateView(tokens, children) {
-    return `
-        <View style={{ ${tokens.map(token => convertTokenToStyle(token)).join(", ")} }} ${!children ? "/" : ""}>
-    `;
+function generateView(tokens, hasChildren) {
+    const styleAttribute = tokens.length ? ` style={{ ${tokens.map(token => convertTokenToStyle(token)).join(", ")} }}` : "";
+    return `<View${styleAttribute}${hasChildren ? "" : "/"}>`;
 }
 
-function compileFromDSL(text) {
+function generateCodeFromParseTree(node) {
+    if (!node.children.length) {
+        return generateView(node.styleTokens);
+    }
+
+    return `${generateView(node.styleTokens, true)}${node.children.map(generateCodeFromParseTree).join("")}</View>`;
+}
+
+class Node {
+    constructor() {
+        this.parent = null;
+        this.children = [];
+        this.styleTokens = [];
+    }
+}
+
+function getParseTree(text) {
     const textTokens = text.split(".").filter(Boolean).map(tokenWithoutDot => `.${tokenWithoutDot}`);
-    let token;
+    const root = new Node();
+    let itNode = root;
 
     function advanceToken() {
-        token = textTokens.shift();
+        const token = textTokens.shift();
         if (!token) {
             return;
         }
 
         switch (token) {
             case tokens.LEFT_BRACKET:
-                leftBracket();
+                leftBracket(token);
                 break;
             case tokens.RIGHT_BRACKET:
-                rightBracket();
+                rightBracket(token);
                 break;
             case tokens.SIBLING_SEP:
-                siblingSep();
+                siblingSep(token);
                 break;
             default:
-                styleToken();
+                styleToken(token);
                 break;
         }
     }
 
     function leftBracket() {
-        console.log("left bracket");
+        const newNode = new Node();
+        newNode.parent = itNode;
+        itNode.children.push(newNode);
+        itNode = newNode;
+
         advanceToken();
     }
 
     function rightBracket() {
-        console.log("right bracket");
+        itNode = itNode.parent;
         advanceToken();
     }
 
     function siblingSep() {
-        console.log("sibling sep");
+        const newNode = new Node();
+        newNode.parent = itNode.parent;
+        itNode.parent.children.push(newNode);
+        itNode = newNode;
+
+        advanceToken();
     }
 
-    function styleToken() {
-        console.log("style token", token);
+    function styleToken(token) {
+        itNode.styleTokens.push(token);
         advanceToken();
     }
 
     advanceToken();
+    return root;
 }
 
-compileFromDSL(example);
+const root = getParseTree(example);
+const code = generateCodeFromParseTree(root);
+console.log(prettier.format(code, { parser: "babel" }));
